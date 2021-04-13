4942cdd457f0857badb4d4d50f88d201079f6b65
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-04-13 19:48:25 -0300

    criando Interação com o Usuário

--- controllers/FilmeController.cs
+++ controllers/FilmeController.cs
@@ -1,7 +1,30 @@
+using System;
+using Repositories;
+
 namespace controllers
 {
     public class FilmeController
     {
-        
+        private FilmeRepository repository = new FilmeRepository();
+
+        public void Processos()
+        {
+
+        }
+
+        private string LerOpcaoUsuario()
+        {
+            Console.WriteLine("Bem vindo(a) a Digital Filmes :");
+            Console.WriteLine("Digite a opcao desejada:");
+            Console.WriteLine("1=> Para Listar Filmes:");
+            Console.WriteLine("2=> Para Buscar Filme");
+            Console.WriteLine("3=> Para Cadastrar Novo Filme:");
+            Console.WriteLine("4=> Para Atualizar Filme:");
+            Console.WriteLine("5=> Para Remover Filme:");
+            Console.WriteLine("L=> Para Limpar a Tela:");
+            Console.WriteLine("S=> Para Sair do Sistema:");
+            string opcao= Console.ReadLine().ToUpper();
+            return opcao;
+        }
     }
 }
