7419354daab99cfc784d8869aa4e56cca4350c5d
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-04-13 19:36:31 -0300

    Criado a migração

---
+++ Migrations/20210413221021_TesteDeMigracao.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace digitalSeries_Projeto.Migrations
+{
+    public partial class TesteDeMigracao : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Filmes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "VARCHAR(80)", nullable: false),
+                    Genero = table.Column<int>(type: "int", nullable: false),
+                    Descricao = table.Column<string>(type: "VARCHAR(200)", nullable: false),
+                    AnoLancamento = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filmes", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Filmes_Nome",
+                table: "Filmes",
+                column: "Nome",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Filmes");
+        }
+    }
+}
