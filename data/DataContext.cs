206f9c11bb12c3082b003992a881c944471472ba
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-04-13 19:13:32 -0300

    Configurado Flunt Api na classe de Contexto para testar n o banco algumas especificações antes de executar os testes no projeto

--- data/DataContext.cs
+++ data/DataContext.cs
@@ -9,7 +9,18 @@ namespace data
          {
            optionsBuilder.UseSqlServer("Server=DESKTOP-0V20B0M\\SQLEXPRESS; Database=DigitalSeries_Projeto; User Id=AlbertoDevTeste; Password=asp02");
         }
-
+          
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           modelBuilder.Entity<Filme>().HasKey(p => p.Id );
+           modelBuilder.Entity<Filme>().HasIndex(p => p.Nome).IsUnique();
+           modelBuilder.Entity<Filme>().Property(p => p.Nome).HasColumnType("VARCHAR(80)").IsRequired();
+           modelBuilder.Entity<Filme>().Property(p => p.Genero).IsRequired();
+           modelBuilder.Entity<Filme>().Property(p => p.Descricao).HasColumnType("VARCHAR(200)").IsRequired();
+            modelBuilder.Entity<Filme>().Property(p => p.AnoLancamento).IsRequired();
+           
+           //HasColumnType("VARCHAR(80)").IsRequired();
+        }  
         public DbSet<Filme> Filmes{ get; set;}
     }
 }
