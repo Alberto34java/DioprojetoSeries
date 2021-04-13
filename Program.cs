33fcdb52d188138b51614eaed887591f8bb6cc9e
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-04-13 18:41:46 -0300

    commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace digitalSeries_Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
