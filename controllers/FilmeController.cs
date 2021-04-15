80453e7099a8e5d244b5c9a5a5bb906ae74d025f
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-04-15 10:45:12 -0300

    Métodos criados

--- controllers/FilmeController.cs
+++ controllers/FilmeController.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Collections.Generic;
+using Models;
 using Repositories;
+using ValueObjects;
 
 namespace controllers
 {
     public class FilmeController
     {
         private FilmeRepository repository = new FilmeRepository();
+        private List<Filme> filmes=new List<Filme>();
         
 
         public void Processos()
@@ -18,18 +22,22 @@ namespace controllers
                     case "1":
                     //LISTAGEM
                         break;
-                        //ListarFilmes();
+                        ListarFilmes();
                     case "2":
                     //BUSCA POR FILME
+                    BuscarFilme();
                     break;
                     case "3":
                     //CADASTRAR NOVO FILME
+                    SalvarFilme();
                     break;
                     case "4":
                     //ATUALIZAR CASO ERRO DE CADASTRO ,EXEMPLO NOME DIGITADO ERRADO
+                    AtualizarFilme();
                     break;
                     case "5":
                     //EXCLUSÃO DE FILMES 
+                    ExcluirFilme();
                     break;
                     case "L":
                     //LIMPAR A TELA 
@@ -40,6 +48,117 @@ namespace controllers
                 
                 
             }
+        }
+
+        private void ExcluirFilme()
+        {
+            Console.WriteLine("Digite o Id do Filme:");
+            int id=Int32.Parse(Console.ReadLine());
+            Filme model=repository.BuscarPorId(id);
+            repository.ExcluirRegistro(id);
+
+           
+            
+        }
+
+        private string AtualizarFilme()
+        {
+              Filme filme;
+               filme=null;
+               Console.WriteLine("Digite o Id do Filme: ");
+               int id=Int32.Parse(Console.ReadLine());
+               Console.WriteLine("Digite o Nome Do Filme: ?");
+               string nome=Console.ReadLine();
+               foreach (int i in Enum.GetValues(typeof(Genero)))
+               {
+                  Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));   
+               }
+               Console.WriteLine("Digite O Genero entre as Opções acima: ");
+               int escolha=Int32.Parse(Console.ReadLine());
+               Genero genero=(Genero) escolha;
+               Console.WriteLine("Digite a Descrição: ");
+               string descricao=Console.ReadLine();
+               Console.WriteLine("Digite o Ano de Lancemento do Filme: ");
+               int ano=Int32.Parse(Console.ReadLine());
+               filme=new Filme(nome,genero,descricao,ano);
+               if(repository.AtualizarRegistro(filme, id))
+               {
+                   return "Ok";
+               }
+               else { 
+                   return "Not Update";
+               }
+        }
+
+        private string SalvarFilme()
+        {
+             string msg= "";
+           try
+           {
+              
+               Filme filme;
+               filme=null;
+               Console.WriteLine("Digite o Nome Do Filme: ?");
+               string nome=Console.ReadLine();
+               foreach (int i in Enum.GetValues(typeof(Genero)))
+               {
+                  Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));   
+               }
+               Console.WriteLine("Digite O Genero entre as Opções acima: ");
+               int escolha=Int32.Parse(Console.ReadLine());
+               Genero genero=(Genero) escolha;
+               Console.WriteLine("Digite a Descrição: ");
+               string descricao=Console.ReadLine();
+               Console.WriteLine("Digite o Ano de Lancemento do Filme: ");
+               int ano=Int32.Parse(Console.ReadLine());
+               filme=new Filme(nome,genero,descricao,ano);
+               if(repository.Salvar(filme))
+                { msg=" Salvo com Sucesso!!"; 
+                   return msg;
+                }
+                else {
+                    Console.WriteLine("Erro :");
+                }
+           }
+           catch 
+           {
+               msg=" Erro ao Salvar!!";
+               
+               return msg;
+           }
+
+            return msg;
+        }
+
+        private Filme BuscarFilme()
+        {
+            try
+            {
+                Console.WriteLine("Informe o Id?");
+                int id=Int32.Parse(Console.ReadLine());
+                var filme=repository.BuscarPorId(id);
+
+                return filme;
+            }
+            catch (System.Exception)
+            {
+                
+                return null;
+            }
+        }
+
+        private List<Filme> ListarFilmes()
+        {
+           try
+           {
+               filmes=repository.Listar();
+               return filmes;
+           }
+           catch 
+           {
+               
+              return null;
+           }
         }
 
         private string LerOpcaoUsuario()
